6f6dd02240bdc2cc3217a681c86ee674fe0e4a73
caifeiyue <caifeiyue1991@163.com>
2018-12-04 20:52:33 +0800

    add move bike

--- birth_wq/fireworks.py
+++ birth_wq/fireworks.py
@@ -9,15 +9,15 @@ from asciimatics.exceptions import ResizeScreenError
 from random import randint, choice
 import sys
 
-from resources.ascii_art import love, hug_two
+from resources.ascii_art import love, hug_two, bike_two
 
 def move_animation(screen, ascii_obj):
-    centre = (screen.width // 2, screen.height // 2 - 3)
+    centre = (screen.width // 2, screen.height // 2 - 10)
     path = Path()
-    # path.jump_to(screen.width + 16, centre[1])
     obj_len = 2
     half_length = 10
-    for i in range(half_length):
+    obj_length = 5
+    """for i in range(half_length):
         path.jump_to(centre[0]-i*2, centre[1]-i)
         path.jump_to(centre[0]+i*2, centre[1]-i)
     for i in range(half_length):
@@ -26,8 +26,10 @@ def move_animation(screen, ascii_obj):
     for i in range(1, 2*half_length-1):
         path.jump_to(centre[0]-(half_length-1)*2*2+i*2, centre[1]+i)
         path.jump_to(centre[0]+(half_length-1)*2*2-i*2, centre[1]+i)
-
-    # path.move_straight_to(screen.width+obj_length, centre[1], (screen.width + obj_length) // 2)
+    """
+    bike_height = 20
+    path.jump_to(-obj_length, bike_height)
+    path.move_straight_to(screen.width, bike_height, (screen.width + obj_length) // 1)
     """effects = [
         Print(screen,
               # FigletText("ASCIIMATICS", font='big'),
@@ -41,8 +43,7 @@ def move_animation(screen, ascii_obj):
                         "default": StaticRenderer(images=[ascii_obj])
                     },
             path=path,
-            colour=255,
-            clear=False)
+            colour=255)
     effects = [sprite]
     # return effects
     return sprite
@@ -84,15 +85,15 @@ def demo(screen):
 
     effects.append(Print(screen,
                          Rainbow(screen, FigletText("HAPPY")),
-                         screen.height // 2 - 6,
+                         screen.height // 2 - 16,
                          speed=1,
                          start_frame=100))
     effects.append(Print(screen,
                          Rainbow(screen, FigletText("BIRTHDAY!")),
-                         screen.height // 2 + 1,
+                         screen.height // 2 - 10,
                          speed=1,
                          start_frame=100))
-    love_move = move_animation(screen, love)
+    love_move = move_animation(screen, bike_two)
     effects.append(love_move)
     scenes.append(Scene(effects, -1))
     # scenes.append(Scene(love_move, -1))
